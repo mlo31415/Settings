78cffa6bdd3f0cbc70a1a7a98e934751262752ca
mlo31415 <mlo@baskerville.org>
2021-11-19 08:15:12 -0500

    Improve type hinting Add default value to Get Reorder methods

--- Settings.py
+++ Settings.py
@@ -1,12 +1,17 @@
 from __future__ import annotations
-from typing import Optional
+from typing import Any
 
 import os
 import json
 
 class Settings():
-    g_settings:dict[str, ]={}
+    g_settings:dict[str, Any]={}
     g_settingsFilename: str=""
+
+    def Save(self) -> None:
+        if len(Settings.g_settingsFilename) > 0:
+            with open(Settings.g_settingsFilename, "w+") as file:
+                file.write(json.dumps(Settings.g_settings))
 
     def Load(self, fname: str) -> None:
         Settings.g_settingsFilename=os.path.join(os.getcwd(), fname)
@@ -16,16 +21,11 @@ class Settings():
                 if len(lines) > 0:
                     Settings.g_settings=json.loads(lines)
 
-    def Put(self, name: str, val) -> None:
+    def Put(self, name: str, val: Any) -> None:
         Settings.g_settings[name]=val
         self.Save()
 
-    def Save(self) -> None:
-        if len(Settings.g_settingsFilename)> 0:
-            with open(Settings.g_settingsFilename, "w+") as file:
-                file.write(json.dumps(Settings.g_settings))
-
-    def Get(self, name: str) -> Optional[str]:
+    def Get(self, name: str, default: Any=None) -> Any:
         if name not in Settings.g_settings.keys():
-            return None
+            return default
         return Settings.g_settings[name]
