75827aa92a0fa89d29ca6b629427e41ccbc41c98
mlo31415 <mlo@baskerville.org>
2022-02-22 10:45:47 -0500

    Add optional better handling of case of missing settings file

--- Settings.py
+++ Settings.py
@@ -4,6 +4,8 @@ from typing import Any
 import os
 import json
 
+from HelpersPackage import MessageBox
+
 class Settings:
     g_settings:dict[str, Any]={}
     g_settingsFilename: str=""
@@ -13,13 +15,20 @@ class Settings:
             with open(Settings.g_settingsFilename, "w+") as file:
                 file.write(json.dumps(Settings.g_settings))
 
-    def Load(self, fname: str) -> None:
+    def Load(self, fname: str, MustExist: bool=False) -> bool:
+        if MustExist and not os.path.exists(fname):
+            MessageBox(f"Settings file {fname} is missing.  This is unlikely to end well...", ignoredebugger=True)
+            return False
+
+        # OK, with the settings file exists or we don't care if it exists or not.
+        # (If it doesn't exist, it will be created on save.)
+        with open(fname, "r") as file:
+            lines=file.read()
+            if len(lines) > 0:
+                Settings.g_settings=json.loads(lines)
         Settings.g_settingsFilename=os.path.join(os.getcwd(), fname)
-        if os.path.exists(fname):
-            with open(fname, "r") as file:
-                lines=file.read()
-                if len(lines) > 0:
-                    Settings.g_settings=json.loads(lines)
+        return True
+
 
     def Put(self, name: str, val: Any) -> None:
         Settings.g_settings[name]=val
